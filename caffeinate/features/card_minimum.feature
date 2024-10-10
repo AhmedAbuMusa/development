4430e0c7d2314e89c0ce9cfe24d9a771a3de4cc2
Ahmed AbuMusa <ahmedabumusa@outlook.com>
2024-10-10 19:55:50 +0300

    complete sample added

--- caffeinate/features/card_minimum.feature
+++ caffeinate/features/card_minimum.feature
@@ -4,3 +4,8 @@ Feature: Card Minimum
         Given Maria orders a $3 coffee
         When Maria pays with a credit card
         Then Lee should process the payment
+
+    Scenario: Total charge is under the $2 credit card minimum
+        Given Maria orders a $1 coffee
+        When Maria pays with a credit card
+        Then Lee should not process the payment
--- caffeinate/features/step_definitions/steps.rb
+++ caffeinate/features/step_definitions/steps.rb
@@ -1,16 +1,34 @@
+require 'caffeinate'
 
-Given("Maria orders a ${int} coffee from Lee") do |int|
-  # Write code here that turns the phrase above into concrete actions
-  maria = Customer.new
-  li = Associate.new
-  price = int
-  maria.order
+Given("Maria orders a ${int} coffee") do |int|
+  @maria = Customer.new
+  @li = Associate.new
+  @price = int
+  @card_minimum = int
+  @maria.orders
 end
 
+# Given("Maria orders a ${int} coffee from Lee") do |int|
+#   @maria = Customer.new
+#   @li = Associate.new
+#   price = int
+#   @maria.orders
+# end
+
 When("Maria pays with a credit card") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @maria.pays_with_credit
 end
 
 Then("Lee should process the payment") do
-  pending # Write code here that turns the phrase above into concrete actions
+  @price = 3
+  @card_minimum = 2
+  expect(@li.process_payment(@price, @card_minimum)).to include(true)
 end
+
+Then("Lee should not process the payment") do
+  @price = 1 
+  @card_minimum = 2
+  expect(@li.process_payment(@price, @card_minimum)).to include(false)
+  expect(@li.process_payment(@price, @card_minimum)).to include('request_cash')
+end
+
